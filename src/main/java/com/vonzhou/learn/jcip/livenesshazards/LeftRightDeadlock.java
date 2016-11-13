588a8b82ef89b0cc69a3f3ecd7c9bfb22cf2b1fd
vonzhou <vonzhou@163.com>
2016-11-13 21:42:11 +0800

    update

---
+++ src/main/java/com/vonzhou/learn/jcip/livenesshazards/LeftRightDeadlock.java
@@ -0,0 +1,35 @@
+package com.vonzhou.learn.jcip.livenesshazards;
+
+/**
+ * LeftRightDeadlock
+ * <p>
+ * Simple lock-ordering deadlock
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class LeftRightDeadlock {
+    private final Object left = new Object();
+    private final Object right = new Object();
+
+    public void leftRight() {
+        synchronized (left) {
+            synchronized (right) {
+                doSomething();
+            }
+        }
+    }
+
+    public void rightLeft() {
+        synchronized (right) {
+            synchronized (left) {
+                doSomethingElse();
+            }
+        }
+    }
+
+    void doSomething() {
+    }
+
+    void doSomethingElse() {
+    }
+}
